765a2e1c461b1c5f04b7c8af6f2c319625550759
sydneywb <badescusydney@gmail.com>
2023-10-31 10:50:07 -0700

    enhanced search
    can now search for words in file path

--- DocSearchServer.java
+++ DocSearchServer.java
@@ -14,17 +14,17 @@ class FileHelpers {
     static List<File> getFiles(Path start) throws IOException {
         File f = start.toFile();
         List<File> result = new ArrayList<>();
-        if(f.isDirectory()) {
+        if (f.isDirectory()) {
             File[] paths = f.listFiles();
-            for(File subFile: paths) {
+            for (File subFile : paths) {
                 result.addAll(getFiles(subFile.toPath()));
             }
-        }
-        else {
+        } else {
             result.add(start.toFile());
         }
         return result;
     }
+
     static String readFile(File f) throws IOException {
         return new String(Files.readAllBytes(f.toPath()));
     }
@@ -32,40 +32,52 @@ class FileHelpers {
 
 class Handler implements URLHandler {
     Path base;
+
     Handler(String directory) throws IOException {
-      this.base = Paths.get(directory);
+        this.base = Paths.get(directory);
     }
+
     public String handleRequest(URI url) throws IOException {
-       List<File> paths = FileHelpers.getFiles(this.base);
-       if (url.getPath().equals("/")) {
-           return String.format("There are %d total files to search.", paths.size());
-       } else if (url.getPath().equals("/search")) {
-           String[] parameters = url.getQuery().split("=");
-           if (parameters[0].equals("q")) {
-               String result = "";
-               List<String> foundPaths = new ArrayList<>();
-               for(File f: paths) {
-                   if(FileHelpers.readFile(f).contains(parameters[1])) {
-                       foundPaths.add(f.toString());
-                   }
-               }
-               Collections.sort(foundPaths);
-               result = String.join("\n", foundPaths);
-               return String.format("Found %d paths:\n%s", foundPaths.size(), result);
-           }
-           else {
-               return "Couldn't find query parameter q";
-           }
-       }
-       else {
-           return "Don't know how to handle that path!";
-       }
+        List<File> paths = FileHelpers.getFiles(this.base);
+        if (url.getPath().equals("/")) {
+            return String.format("There are %d total files to search.", paths.size());
+        } else if (url.getPath().equals("/search")) {
+            String[] parameters = url.getQuery().split("=");
+            if (parameters[0].equals("q")) {
+                String result = "";
+                List<String> foundPaths = new ArrayList<>();
+                for (File f : paths) {
+                    if (FileHelpers.readFile(f).contains(parameters[1])) {
+                        foundPaths.add(f.toString());
+                    }
+                }
+                Collections.sort(foundPaths);
+                result = String.join("\n", foundPaths);
+                return String.format("Found %d paths:\n%s", foundPaths.size(), result);
+            } else if (parameters[0].equals("title")) {
+                String result = "";
+
+                // return all the file paths where the given string is part of the path of the file
+                List<String> foundPaths = new ArrayList<> ();
+                for (File f: paths) {
+                    if (f.toString().contains(parameters[1])) {
+                        foundPaths.add(f.toString());
+                    }
+                }
+                result = String.join("\n", foundPaths);
+                return String.format("Hey, I found %d files with %s in the path:\n%s", foundPaths.size(), parameters[1], result);
+            } else {
+                return "Couldn't find query parameter q";
+            }
+        } else {
+            return "Don't know how to handle that path!";
+        }
     }
 }
 
 class DocSearchServer {
     public static void main(String[] args) throws IOException {
-        if(args.length == 0){
+        if (args.length == 0) {
             System.out.println("Missing port number! Try any number between 1024 to 49151");
             return;
         }
@@ -75,4 +87,3 @@ class DocSearchServer {
         Server.start(port, new Handler(args[1]));
     }
 }
-
--- TestDocSearch.java
+++ TestDocSearch.java
@@ -2,21 +2,39 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.beans.Transient;
 import java.io.IOException;
 
 public class TestDocSearch {
-	@Test 
-	public void testIndex() throws URISyntaxException, IOException {
-    Handler h = new Handler("./technical/");
-    URI rootPath = new URI("http://localhost/");
-    assertEquals("There are 1391 total files to search.", h.handleRequest(rootPath));
-	}
-	@Test 
-	public void testSearch() throws URISyntaxException, IOException {
-    Handler h = new Handler("./technical/");
-    URI rootPath = new URI("http://localhost/search?q=Resonance");
-    String expect = "Found 2 paths:\n./technical/biomed/ar615.txt\n./technical/plos/journal.pbio.0020150.txt";
-    assertEquals(expect, h.handleRequest(rootPath));
-	}
-}
+    @Test
+    public void testIndex() throws URISyntaxException, IOException {
+        Handler h = new Handler("./technical/");
+        URI rootPath = new URI("http://localhost/");
+        assertEquals("There are 1394 total files to search.", h.handleRequest(rootPath));
+    }
 
+    @Test
+    public void testSearch() throws URISyntaxException, IOException {
+        Handler h = new Handler("./technical/");
+        URI rootPath = new URI("http://localhost/search?q=Resonance");
+        String expect = "Found 2 paths:\n./technical/biomed/ar615.txt\n./technical/plos/journal.pbio.0020150.txt";
+        assertEquals(expect, h.handleRequest(rootPath));
+    }
+
+    @Test // careful about spaces in actual versus expected output
+    public void testTitle() throws URISyntaxException, IOException {
+        Handler h = new Handler("./technical/");
+        URI rootPath = new URI("http://localhost/search?title=nicole");
+        // when printing paths to webpage, seems that there's actually a ./ at the start of the path
+        // so when testing, make sure to put ./ at the beginning of the expected string
+        String expect = "Hey, I found 1 files with nicole in the path:\n./technical/plos/nicole.txt";
+        assertEquals(expect, h.handleRequest(rootPath));
+    }
+    @Test
+    public void testTitle2() throws URISyntaxException, IOException {
+        Handler h = new Handler("./technical/");
+        URI rootPath = new URI("http://localhost/search?title=cinn");
+        String expect = "Hey, I found 1 files with cinn in the path:\n./technical/cinnamon/baking.txt";
+        assertEquals(expect, h.handleRequest(rootPath));
+    }
+}
